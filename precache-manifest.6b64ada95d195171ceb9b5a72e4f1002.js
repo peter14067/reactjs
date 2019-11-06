dfc55f01e051e9b45f58efe141561e4f58ab7971
peter14067 <peter14067@gamil.com>
2019-11-06 08:43:19 +0800

    Updates

---
+++ precache-manifest.6b64ada95d195171ceb9b5a72e4f1002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bf8625cb8a206f1337af405b6b89aa",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "0494197c951dfe9837aa",
+    "url": "/reactjs/static/css/main.a8719ca5.chunk.css"
+  },
+  {
+    "revision": "3efd3b35cf9836fc9b33",
+    "url": "/reactjs/static/js/2.8c762f73.chunk.js"
+  },
+  {
+    "revision": "0494197c951dfe9837aa",
+    "url": "/reactjs/static/js/main.64b78992.chunk.js"
+  },
+  {
+    "revision": "46c91ca38b47c53ccba4",
+    "url": "/reactjs/static/js/runtime-main.5a45c505.js"
+  }
+]);
