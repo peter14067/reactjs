fde28c27669a7c1dcd90fc52e93bed645bed7e0e
peter14067 <peter14067@gmail.com>
2019-11-14 22:19:45 +0800

    Updates

--- precache-manifest.72045fc69661eed660c651ab3207b9a9.js
+++ precache-manifest.72045fc69661eed660c651ab3207b9a9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af396c1b545f7f89477168af2f2e0019",
+    "revision": "bb8ab45f6784ca225781037ed291d325",
     "url": "/reactjs/index.html"
   },
   {
-    "revision": "041dc3ae1ec7ec0f8caa",
-    "url": "/reactjs/static/css/main.7cbaea60.chunk.css"
+    "revision": "2a30ce61993068737f93",
+    "url": "/reactjs/static/css/main.a17c2ffc.chunk.css"
   },
   {
     "revision": "3efd3b35cf9836fc9b33",
     "url": "/reactjs/static/js/2.8c762f73.chunk.js"
   },
   {
-    "revision": "041dc3ae1ec7ec0f8caa",
-    "url": "/reactjs/static/js/main.930ea01e.chunk.js"
+    "revision": "2a30ce61993068737f93",
+    "url": "/reactjs/static/js/main.5d13a047.chunk.js"
   },
   {
     "revision": "46c91ca38b47c53ccba4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs/precache-manifest.2379dc168a65c4a7940632f7fa4c5736.js"
+  "/reactjs/precache-manifest.72045fc69661eed660c651ab3207b9a9.js"
 );
 
 self.addEventListener('message', (event) => {
