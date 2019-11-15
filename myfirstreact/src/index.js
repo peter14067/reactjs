af8e57166e8e6346120f99b696cda9939294593a
peter14067 <peter14067@gmail.com>
2019-11-15 19:41:40 +0800

    myfirstreact

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -636,7 +636,7 @@ componentWillUnmount() {
 
         
         <h1>中獎金額</h1>
-        <div class="bonus1">{this.state.bonus}</div>
+        <div>{this.state.bonus}</div>
          
         <br></br>
         <br></br>
